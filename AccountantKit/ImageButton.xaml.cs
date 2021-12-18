d043fd8e68dd5063eff0170fdde7dcfd11151002
jihuaquan <youdonotkonw@163.com>
2021-12-18 15:08:17 +0800

    新增了ImageButton，准备将Image模拟的按钮改为ImageButton控件

---
+++ AccountantKit/ImageButton.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AccountantKit
+{
+    /// <summary>
+    /// ImageButton.xaml 的交互逻辑
+    /// </summary>
+    public partial class ImageButton : Button
+    {
+        private static ImageButton button;
+
+        public ImageButton()
+        {
+            InitializeComponent();
+        }
+
+        public string ImageUri
+        {
+            get { return (string)GetValue(ImageUriProperty); }
+            set { SetValue(ImageUriProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ImageUri.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty ImageUriProperty =
+            DependencyProperty.Register("ImageUri", typeof(string), typeof(ImageButton), new PropertyMetadata(new PropertyChangedCallback(ChangeImageSource)));
+
+        public static void ChangeImageSource(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            button = (ImageButton)d;
+            string uriString = e.NewValue.ToString();
+            button.mainImage.Source = BitmapFrame.Create(new Uri(uriString, UriKind.Relative));
+        }
+    }
+}
